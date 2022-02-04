52c9fc4c91599d36c9d61bbfb7cc71b826430317
Dogtooth <ryanjfinn@gmail.com>
2022-02-03 22:43:36 -0500

    Updated index.js

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
+const crypto = require('crypto');
+const algorithm = 'aes-256-cbc';
+const key = 'sudgpwaupuwp98whEF98WHE9Fhvquhfo'; //**
+const iv = 'jdtbflihbdmygnkd'; //**
+
+function encrypt(text) {
+  let cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(key), iv);
+  let encrypted = cipher.update(text);
+  encrypted = Buffer.concat([encrypted, cipher.final()]);
+  return { iv: iv.toString('hex'), encryptedData: encrypted.toString('hex') };
+}
+
+
+function decrypt(text) {
+  let iv = Buffer.from(text.iv, 'hex');
+  let encryptedText = Buffer.from(text.encryptedData, 'hex');
+  let decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(key), iv);
+  let decrypted = decipher.update(encryptedText);
+  decrypted = Buffer.concat([decrypted, decipher.final()]);
+  return decrypted.toString();
+}
+
 const prompt = require('prompt');
 const {Client, Intents} = require('discord.js');
 const {token, threadId, threadIdTest, version} = require('./config.json');
 const myIntents = new Intents(); myIntents.add(Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_BANS, Intents.FLAGS.GUILDS);
 const client = new Client({ intents: myIntents });
 const channel = client.channels.cache.get(thread);
+
 
 var thread;
 var iD;
@@ -36,6 +59,12 @@ client.once('ready', message =>{
         if (result.msg === 'exit'){
           return process.exit();
         }
+        else if (result.msg.charAt(0) === '*' && result.msg.charAt(1) === '*') {
+          var message = '**' + JSON.stringify(encrypt(result.msg));
+
+          console.log(JSON.stringify(encrypt(result.msg)));
+          channel.send(message);
+        }
         else {
         channel.send(result.msg);
         recursiveReadline();
@@ -50,8 +79,12 @@ client.once('ready', message =>{
 
 client.on('messageCreate', messageCreate => {
   const channel = client.channels.cache.get(thread);
-
   console.log(messageCreate.content);
+if (messageCreate.content.charAt(0) === '*' && messageCreate.content.charAt(1) === '*' ){
+  console.log(decrypt(messageCreate.content.));
+  //console.log(decrypt())
+}
+
 });
 
 client.on('interactionCreate', async interaction => {
@@ -59,7 +92,7 @@ client.on('interactionCreate', async interaction => {
   const {ping} = interaction;
 
   if('ping' === 'ping'){
-    await interaction.reply('pong');
+    await interaction.reply('No!');
   }
 
 });
