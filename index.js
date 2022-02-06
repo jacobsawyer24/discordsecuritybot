d81cbb756e6cb4995bf1159288224cf249340a91
Dogtooth <ryanjfinn@gmail.com>
2022-02-06 00:08:15 -0500

    updated key data type

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ var thread;
 
 function encrypt(text) {
 
-  let cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(key), iv);
+  let cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(key, 'hex'), iv);
   let encrypted = cipher.update(text);
   encrypted = Buffer.concat([encrypted, cipher.final()]);
   iv = crypto.randomBytes(16);
@@ -26,7 +26,7 @@ function encrypt(text) {
 function decrypt(text) {
   let iv = Buffer.from(text.iv, 'hex');;
   let encryptedText = Buffer.from(text.encryptedData, 'hex');
-  let decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(key), iv);
+  let decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(key, 'hex'), iv);
   let decrypted = decipher.update(encryptedText);
   decrypted = Buffer.concat([decrypted, decipher.final()]);
   return decrypted.toString();
